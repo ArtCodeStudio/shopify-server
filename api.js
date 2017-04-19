fa18f7ba11b54a13067f7848a3b429e15873b525
JumpLink <pascal@jumplink.eu>
2017-04-19 13:51:53 +0200

    ported more functions from microservice

--- api.js
+++ api.js
@@ -1,8 +1,9 @@
 'use strict'
 
-const ShopifyApi = require('shopify-api-node'); // https://github.com/microapps/Shopify-api-node
-const Debug = require('debug');                  // https://github.com/visionmedia/debug
-const pTimes = require('p-times');
+const ShopifyApi = require('shopify-api-node');   // https://github.com/microapps/Shopify-api-node
+const Debug = require('debug');                   // https://github.com/visionmedia/debug
+const pTimes = require('p-times');                // https://github.com/sindresorhus/p-times
+const pMap = require('p-map');                    // https://github.com/sindresorhus/p-map
 
 const utilities = require('./utilities.js');
 
@@ -51,6 +52,37 @@ var parseJsonQuery = (jsonQueryString, methodParsedArgs) => {
 }
 
 /**
+ * Custom Api implementations for metafields 
+ */
+api.metafield = {};
+
+/**
+ * Delete multiple metafields at once
+ * TODO TESTME: This function is not tested, just ported from https://git.mediamor.de/jumplink.eu/microservice-shopify/src/master/microservice-shopify.js#L134
+ */
+api.metafield.deleteAll = (shopify, ids) => {
+  return pMap(ids, (id, index) => {
+    return shopify.metafield.delete(id)
+  });
+}
+
+/**
+ * Update multiple metafields at once
+ * TODO TESTME: This function is not tested, just ported from https://git.mediamor.de/jumplink.eu/microservice-shopify/src/master/microservice-shopify.js#L164
+ */
+api.metafield.updateAll = (shopify, metafields) => {
+  return pMap(metafields, (metafield, index) => {
+    var metafield = {
+      id: metafield.id,
+      value: metafield.value,
+      value_type: metafield.value_type,
+    }
+    // console.log(metafield.id, metafield);
+    return shopify.metafield.update(metafield.id, metafield)
+  });
+}
+
+/**
  * Custom Api implementations for products 
  */
 api.product = {};
@@ -59,11 +91,10 @@ api.product = {};
  * Custom Api implementation to get all products at once without pagination
  */
 api.product.getAll = (shopify) => {
-  const productsPerPage = 250;
-
+  const itemsPerPage = 250;
   return shopify.product.count()
   .then((count) => {
-    var pages = Math.round(count / productsPerPage);
+    var pages = Math.round(count / itemsPerPage);
     if(pages <= 0) {
       pages = 1;
     }
@@ -74,12 +105,108 @@ api.product.getAll = (shopify) => {
     return pTimes(pages, (n) => {
       n += 1;
       api.debug("page", n);
-      return shopify.product.list({limit: productsPerPage, page: n})
+      return shopify.product.list({limit: itemsPerPage, page: n})
     });
   })
-  .then((productsOfProducts) => {
-    var products = utilities.flattenArrayOfArray(productsOfProducts);
-    return products;
+  .then((itemsOfItems) => {
+    var items = utilities.flattenArrayOfArray(itemsOfItems);
+    return items;
+  });
+}
+
+/**
+ * Custom Api implementations for customers 
+ */
+api.customer = {};
+
+/**
+ * Custom Api implementation to get all customers at once without pagination
+ */
+api.customer.listAll = (shopify, metafields) => {
+  const itemsPerPage = 250;
+  return shopify.customer.count()
+  .then((count) => {
+    var pages = Math.round(count / itemsPerPage);
+    if(pages <= 0) {
+      pages = 1;
+    }
+
+    api.debug("count", count);
+    api.debug("pages", pages);
+
+    return pTimes(pages, (n) => {
+      n += 1;
+      api.debug("page", n);
+      return shopify.customer.list({limit: itemsPerPage, page: n})
+    });
+  })
+  .then((itemsOfItems) => {
+    var items = utilities.flattenArrayOfArray(itemsOfItems);
+    return items;
+  });
+}
+
+/**
+ * Custom Api implementations for smartCollection 
+ */
+api.smartCollection = {};
+
+/**
+ * Custom Api implementation to get all smartCollection at once without pagination
+ */
+api.smartCollection.listAll = (shopify, metafields) => {
+  const itemsPerPage = 250;
+  return shopify.smartCollection.count()
+  .then((count) => {
+    var pages = Math.round(count / itemsPerPage);
+    if(pages <= 0) {
+      pages = 1;
+    }
+
+    api.debug("count", count);
+    api.debug("pages", pages);
+
+    return pTimes(pages, (n) => {
+      n += 1;
+      api.debug("page", n);
+      return shopify.smartCollection.list({limit: itemsPerPage, page: n})
+    });
+  })
+  .then((itemsOfItems) => {
+    var items = utilities.flattenArrayOfArray(itemsOfItems);
+    return items;
+  });
+}
+
+/**
+ * Custom Api implementations for customCollection 
+ */
+api.customCollection = {};
+
+/**
+ * Custom Api implementation to get all customCollection at once without pagination
+ */
+api.customCollection.listAll = (shopify, metafields) => {
+  const itemsPerPage = 250;
+  return shopify.customCollection.count()
+  .then((count) => {
+    var pages = Math.round(count / itemsPerPage);
+    if(pages <= 0) {
+      pages = 1;
+    }
+
+    api.debug("count", count);
+    api.debug("pages", pages);
+
+    return pTimes(pages, (n) => {
+      n += 1;
+      api.debug("page", n);
+      return shopify.customCollection.list({limit: itemsPerPage, page: n})
+    });
+  })
+  .then((itemsOfItems) => {
+    var items = utilities.flattenArrayOfArray(itemsOfItems);
+    return items;
   });
 }
 
@@ -286,12 +413,92 @@ api.koa = (opts, app) => {
   });
 
   /**
+   * Custom Api metafield/deleteAll implementation
+   * @see api.metafield.updateAll
+   */
+  var url = `${opts.baseUrl}/metafield/deleteAll`;
+  api.debug(`init route: ${url}`);
+  router.get(url, async (ctx) => {
+    const appName = opts.appName;
+    const shopName = ctx.params.shopName;
+    const session = ctx[opts.contextStorageKey];
+    const metafields = req.query.metafields;
+
+    if(!session[appName] || !session[appName][shopName] || !session[appName][shopName].shopifyToken) {
+      ctx.throw(401, 'Shopify Token not set');
+    }
+
+    if(ctx.query === null || typeof(ctx.query) !== 'object' || typeof(ctx.query.json) !== 'string') {
+      api.debug(`ctx.query`, ctx.query);
+      ctx.throw(401, 'Json query string is required');
+    }
+
+    const params = JSON.parse(ctx.query.json);
+
+    if(params === null || typeof(params) !== 'object' || !Array.isArray(params.ids) ) {
+      api.debug(`ctx.query`, ctx.query);
+      ctx.throw(401, 'ids property required and needs to be an array');
+    }
+
+    // TODO: Do not init the api each request!
+    const shopify = api.init(shopName, session[appName][shopName].shopifyToken);
+
+    await api.metafield.deleteAll(shopify, params.ids)
+    .then((metafields) => {
+      ctx.jsonp = metafields;
+    })
+    .catch((error) => {
+      ctx.throw(500, error);
+    });
+  });
+
+  /**
+   * Custom Api metafield/updateAll implementation
+   * @see api.metafield.updateAll
+   */
+  var url = `${opts.baseUrl}/metafield/updateAll`;
+  api.debug(`init route: ${url}`);
+  router.get(url, async (ctx) => {
+    const appName = opts.appName;
+    const shopName = ctx.params.shopName;
+    const session = ctx[opts.contextStorageKey];
+    const metafields = req.query.metafields;
+
+    if(!session[appName] || !session[appName][shopName] || !session[appName][shopName].shopifyToken) {
+      ctx.throw(401, 'Shopify Token not set');
+    }
+
+    if(ctx.query === null || typeof(ctx.query) !== 'object' || typeof(ctx.query.json) !== 'string') {
+      api.debug(`ctx.query`, ctx.query);
+      ctx.throw(401, 'Json query string is required');
+    }
+
+    const params = JSON.parse(ctx.query.json);
+
+    if(params === null || typeof(params) !== 'object' || !Array.isArray(params.metafields) ) {
+      api.debug(`ctx.query`, ctx.query);
+      ctx.throw(401, 'metafields property required and needs to be an array');
+    }
+
+    // TODO: Do not init the api each request!
+    const shopify = api.init(shopName, session[appName][shopName].shopifyToken);
+
+    await api.metafield.updateAll(shopify, params.metafields)
+    .then((metafields) => {
+      ctx.jsonp = metafields;
+    })
+    .catch((error) => {
+      ctx.throw(500, error);
+    });
+  });
+
+  /**
    * Custom Api product/listAll implementation
+   * @see api.product.listAll
    */
   var url = `${opts.baseUrl}/product/listAll`;
   api.debug(`init route: ${url}`);
   router.get(url, async (ctx) => {
-    const productsPerPage = 250;
     const appName = opts.appName;
     const shopName = ctx.params.shopName;
     const session = ctx[opts.contextStorageKey];
@@ -303,9 +510,90 @@ api.koa = (opts, app) => {
     // TODO: Do not init the api each request!
     const shopify = api.init(shopName, session[appName][shopName].shopifyToken);
 
-    await api.product.getAll(shopify)
-    .then((products) => {
-      ctx.jsonp = products;
+    await api.product.listAll(shopify)
+    .then((results) => {
+      ctx.jsonp = results;
+    })
+    .catch((error) => {
+      ctx.throw(500, error);
+    });
+  });
+
+  /**
+   * Custom Api customer/listAll implementation
+   * @see api.customer.listAll
+   */
+  var url = `${opts.baseUrl}/customer/listAll`;
+  api.debug(`init route: ${url}`);
+  router.get(url, async (ctx) => {
+    const appName = opts.appName;
+    const shopName = ctx.params.shopName;
+    const session = ctx[opts.contextStorageKey];
+
+    if(!session[appName] || !session[appName][shopName] || !session[appName][shopName].shopifyToken) {
+      ctx.throw(401, 'Shopify Token not set');
+    }
+
+    // TODO: Do not init the api each request!
+    const shopify = api.init(shopName, session[appName][shopName].shopifyToken);
+
+    await api.customer.listAll(shopify)
+    .then((results) => {
+      ctx.jsonp = results;
+    })
+    .catch((error) => {
+      ctx.throw(500, error);
+    });
+  });
+
+  /**
+   * Custom Api smartCollection/listAll implementation
+   * @see api.smartCollection.listAll
+   */
+  var url = `${opts.baseUrl}/smartCollection/listAll`;
+  api.debug(`init route: ${url}`);
+  router.get(url, async (ctx) => {
+    const appName = opts.appName;
+    const shopName = ctx.params.shopName;
+    const session = ctx[opts.contextStorageKey];
+
+    if(!session[appName] || !session[appName][shopName] || !session[appName][shopName].shopifyToken) {
+      ctx.throw(401, 'Shopify Token not set');
+    }
+
+    // TODO: Do not init the api each request!
+    const shopify = api.init(shopName, session[appName][shopName].shopifyToken);
+
+    await api.smartCollection.listAll(shopify)
+    .then((results) => {
+      ctx.jsonp = results;
+    })
+    .catch((error) => {
+      ctx.throw(500, error);
+    });
+  });
+
+  /**
+   * Custom Api customCollection/listAll implementation
+   * @see api.customCollection.listAll
+   */
+  var url = `${opts.baseUrl}/customCollection/listAll`;
+  api.debug(`init route: ${url}`);
+  router.get(url, async (ctx) => {
+    const appName = opts.appName;
+    const shopName = ctx.params.shopName;
+    const session = ctx[opts.contextStorageKey];
+
+    if(!session[appName] || !session[appName][shopName] || !session[appName][shopName].shopifyToken) {
+      ctx.throw(401, 'Shopify Token not set');
+    }
+
+    // TODO: Do not init the api each request!
+    const shopify = api.init(shopName, session[appName][shopName].shopifyToken);
+
+    await api.customCollection.listAll(shopify)
+    .then((results) => {
+      ctx.jsonp = results;
     })
     .catch((error) => {
       ctx.throw(500, error);
@@ -314,7 +602,6 @@ api.koa = (opts, app) => {
 
   /*
    * Init all routes for the Shopify REST API based on Shopify-api-node
-   * 
    * @see https://github.com/microapps/Shopify-api-node
    */
   utilities.async.forEach(api.definitions, (resourceName, resource, next) => {
