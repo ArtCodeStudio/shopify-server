ce37eb28efb7abc6e00d2f209185d28a0fbaf1c1
JumpLink <pascal@jumplink.eu>
2017-05-02 15:22:33 +0200

    use class

--- admin.js
+++ admin.js
@@ -1,53 +1,58 @@
-/*jslint es6 */
-
-/**
- * Node modul for admin stuff, e.g list all shops where the app is installed,
- * so this should not be accessable by single app users, just for admins
- */
-
 'use strict';
 
+  /*!
+  * Module dependencies.
+  */
 const Debug = require('debug');                 // https://github.com/visionmedia/debug
 const utilities = require('./utilities.js');
 
-var admin = {
-  debug: new Debug('shopify-server:admin')
-};
-
-admin.shop = {
-
-}
-
 /**
- * Get all shops where the app is installed
+ * Class for admin stuff, e.g list all shops where the app is installed,
+ * so this should not be accessable by single app users, just for admins
  */
-admin.shop.list = (firebaseApp) => {
-    return new Promise((fulfill, reject) => {
-        var db = firebaseApp.database();
-        var ref = db.ref('/shopifyAccessToken');
-        ref.once('value', (snapshot) => {
-            var values = snapshot.val(); // null if empty
-            admin.debug('admin.shop.list: ', values);
-            var splits = [];
-            if (values !== null) {
-              Object.keys(values).map((key, index) => {
-                  var shopData = utilities.getShopByFirebaseUID(key);
-                  var split = {
-                      uid: key,
-                      shop: shopData.shop,
-                      shopName: shopData.shopName,
-                      accessToken: values[key]
-                  } 
-                  splits.push(split);
-                  admin.debug(split);
-              });
-            }
-            fulfill(splits); 
-        }, (errorObject) => {
-            admin.debug(errorObject);
-            reject(errorObject);
+class Admin {
+
+    /**
+     * Create an admin object
+     */
+    constructor() {
+        this.debug = new Debug('shopify-server:admin');
+    };
+
+    /**
+     * Get all shops where the app is installed
+     * @param {Object} firebaseApp
+     * @return {Promise<Array>} - A promise to an array of shop datas object
+     */
+    list(firebaseApp) {
+        return new Promise((fulfill, reject) => {
+            let db = firebaseApp.database();
+            let ref = db.ref('/shopifyAccessToken');
+            ref.once('value', (snapshot) => {
+                let values = snapshot.val(); // null if empty
+                this.debug('admin.shop.list: ', values);
+                let splits = [];
+                if (values !== null) {
+                Object.keys(values).map((key, index) => {
+                    let shopData = utilities.getShopByFirebaseUID(key);
+                    let split = {
+                        uid: key,
+                        shop: shopData.shop,
+                        shopName: shopData.shopName,
+                        accessToken: values[key],
+                    };
+                    splits.push(split);
+                    this.debug(split);
+                });
+                }
+                fulfill(splits);
+            }, (errorObject) => {
+                this.debug(errorObject);
+                reject(errorObject);
+            });
         });
-    });
+    };
+
 }
 
-module.exports = admin;
+module.exports = Admin;
